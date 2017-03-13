93056d00e776beb579b90d970afc2574e74c0cf3
Jerrif Chinn <j_man_9999@hotmail.com>
2017-03-13 14:54:05 +0000

    Initial commit

---
+++ string.s
@@ -0,0 +1,79 @@
+	.text
+	.file	"string.c"
+	.globl	main
+	.align	16, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# BB#0:
+	pushq	%rbp
+.Ltmp0:
+	.cfi_def_cfa_offset 16
+.Ltmp1:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+.Ltmp2:
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	callq	get_string
+	movq	%rax, -8(%rbp)
+	movq	-8(%rbp), %rdi
+	callq	print_name
+	xorl	%eax, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+.Ltmp3:
+	.size	main, .Ltmp3-main
+	.cfi_endproc
+
+	.globl	print_name
+	.align	16, 0x90
+	.type	print_name,@function
+print_name:                             # @print_name
+	.cfi_startproc
+# BB#0:
+	pushq	%rbp
+.Ltmp4:
+	.cfi_def_cfa_offset 16
+.Ltmp5:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+.Ltmp6:
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movabsq	$.L.str, %rax
+	movq	%rdi, -8(%rbp)
+	movq	-8(%rbp), %rsi
+	movq	%rax, %rdi
+	movb	$0, %al
+	callq	printf
+	movl	%eax, -12(%rbp)         # 4-byte Spill
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+.Ltmp7:
+	.size	print_name, .Ltmp7-print_name
+	.cfi_endproc
+
+	.type	.L.str,@object          # @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	"hello, %s\n"
+	.size	.L.str, 11
+
+	.type	GetChar,@object         # @GetChar
+	.comm	GetChar,8,8
+	.type	GetDouble,@object       # @GetDouble
+	.comm	GetDouble,8,8
+	.type	GetFloat,@object        # @GetFloat
+	.comm	GetFloat,8,8
+	.type	GetInt,@object          # @GetInt
+	.comm	GetInt,8,8
+	.type	GetLongLong,@object     # @GetLongLong
+	.comm	GetLongLong,8,8
+	.type	GetString,@object       # @GetString
+	.comm	GetString,8,8
+
+	.ident	"Ubuntu clang version 3.6.0-2ubuntu1~trusty1 (tags/RELEASE_360/final) (based on LLVM 3.6.0)"
+	.section	".note.GNU-stack","",@progbits
